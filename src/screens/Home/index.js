fb9f959842278fd9594b24c4feafae4f2395fabf
Scott Henshaw <scott.henshaw86@gmail.com>
2021-01-28 18:42:11 +0900

    swipe to next post

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,44 +1,20 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, FlatList, Dimensions } from 'react-native';
 
 import Post from '../../components/Post';
-
-const post1 = {
-    id: 'p1',
-    videoUri: 'https://scottsimagesandvideos.s3.ap-northeast-2.amazonaws.com/videos/bowling.mp4',
-    user: {
-        id: 'u1',
-        username: 'scotthenshaw',
-        imageUri: 'https://scottsimagesandvideos.s3.ap-northeast-2.amazonaws.com/images/harvey.jpg',
-    },
-    description: 'bowling! hahaha @harvey',
-    songName: 'baby bowling - Scott',
-    songImage: 'https://scottsimagesandvideos.s3.ap-northeast-2.amazonaws.com/images/harvey.jpg',
-    likes: 123,
-    comments: 12,
-    shares: 44,
-};
-
-const post2 = {
-    id: 'p2',
-    videoUri: 'https://scottsimagesandvideos.s3.ap-northeast-2.amazonaws.com/videos/harv.mp4',
-    user: {
-        id: 'u2',
-        username: 'scotthenshaw',
-        imageUri: 'https://scottsimagesandvideos.s3.ap-northeast-2.amazonaws.com/images/harvey.jpg',
-    },
-    description: "Harvey's toy bus @harvey",
-    songName: 'little blue bus - Tayo',
-    songImage: 'https://scottsimagesandvideos.s3.ap-northeast-2.amazonaws.com/images/harvey.jpg',
-    likes: 212,
-    comments: 64,
-    shares: 97,
-};
+import posts from '../../data/posts';
 
 const Home = (props) => {
     return (
         <View style={styles.container}>
-            <Post post={post1}/>
+            <FlatList 
+                data={posts}
+                renderItem={({item}) => <Post post={item}/>}
+                showsVerticalScrollIndicator={false}
+                snapToInterval={Dimensions.get('window').height}
+                snapToAlignment={"start"}
+                decelerationRate={"fast"}
+            />
         </View>
     );
 };
